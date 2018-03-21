28603c8d2d8e60411526d4bdd3b8009f0b8127b1
Xiao-Yong Jin <xjin@anl.gov>
2018-03-20 22:15:12 -0500

    acme: fix some memory leaks

--- src/cmd/acme/acme.c
+++ src/cmd/acme/acme.c
@@ -390,7 +390,7 @@ int erroutfd;
 void
 acmeerrorproc(void *v)
 {
-	char *buf;
+	char *buf, *s;
 	int n;
 
 	USED(v);
@@ -398,8 +398,11 @@ acmeerrorproc(void *v)
 	buf = emalloc(8192+1);
 	while((n=read(errorfd, buf, 8192)) >= 0){
 		buf[n] = '\0';
-		sendp(cerr, estrdup(buf));
+		s = estrdup(buf);
+		sendp(cerr, s);
+		free(s);
 	}
+	free(buf);
 }
 
 void
