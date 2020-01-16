f242601b3d270b8551ecb7cb13485052b30a270c
Xiao-Yong Jin <xjin@anl.gov>
2020-01-16 09:58:50 -0600

    Revert "acme: allow multi-line window tag dump/load encoded with unicode PUA"
    This reverts commit e74e67d00849af9159fc73d188be752ec9e67d50.

--- src/cmd/acme/rows.c
+++ src/cmd/acme/rows.c
@@ -22,9 +22,6 @@ static Rune Lcolhdr[] = {
 	0
 };
 
-/* Use unicode PUA to (un)mask ascii */
-#define PMASK(x) (0XF000^x)
-
 void
 rowinit(Row *row, Rectangle r)
 {
@@ -436,10 +433,10 @@ rowdump(Row *row, char *file)
 			Bwrite(b, buf, strlen(buf));
 			m = min(RBUFSIZE, w->tag.file->b.nc);
 			bufread(&w->tag.file->b, 0, r, m);
-			for(n=0;n<m;n++)
-				if(r[n]=='\n')
-					r[n] = PMASK('\n');
-			Bprint(b, "%.*S\n", m, r);
+			n = 0;
+			while(n<m && r[n]!='\n')
+				n++;
+			Bprint(b, "%.*S\n", n, r);
 			if(dumped){
 				q0 = 0;
 				q1 = t->file->b.nc;
@@ -736,9 +733,6 @@ rowload(Row *row, char *file, int initing)
 			if(r[n] == '|')
 				break;
 		wincleartag(w);
-		for(i=n;i<nr;i++)
-			if(r[i] == PMASK('\n'))
-				r[i] = '\n';
 		textinsert(&w->tag, w->tag.file->b.nc, r+n+1, nr-(n+1), TRUE);
 		if(ndumped >= 0){
 			/* simplest thing is to put it in a file and load that */
